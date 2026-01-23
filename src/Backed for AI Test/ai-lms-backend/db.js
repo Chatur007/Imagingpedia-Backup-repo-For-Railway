2fea281612c7290a374f9e2265b986a9f0cfb61b
Chatur <cheturshankar2002@gmail.com>
2026-01-23 17:35:19 +0530

    made changes in .env and db.js files for deployement

--- src/Backed for AI Test/ai-lms-backend/db.js
+++ src/Backed for AI Test/ai-lms-backend/db.js
@@ -2,13 +2,18 @@ import pkg from 'pg';
 
 const {Pool} =pkg;
 
-export const pool=new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"lms",
-    password:"chatur",
-    port:5432,
-}) ;
+// export const pool=new Pool({
+//     user:"postgres",
+//     host:"localhost",
+//     database:"lms",
+//     password:"chatur",
+//     port:5432,
+// }) ;
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
 
 pool.on("connect", () => {
  console.log("PostgreSQL connected");
